efed819b703798355d7239dbd32b78b112aabe75
Krisztian <fodorkrisztian6@gmail.com>
2021-02-06 11:09:20 +0100

    MZo4 (nem megfeleloen mukodik), MZo5

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,15 @@
 //can be deleted
 console.log("Console log works!");
-let form
+
+//create js element
+
 let InputElement = document.getElementById("todo-text");
+let CreateButton = document.getElementById("create-btn");
 
-let CreateButton = document.getElementById("create-btn")
-
-let UlElement = document.getElementById("ul-element")
+// create and remove list items
 
 CreateButton.addEventListener("click", event =>{
     event.preventDefault();
-    console.log(InputElement.value);
     let liElement = document.createElement("li");
     document.body.append(liElement);
     liElement.innerText=InputElement.value;
@@ -17,11 +17,23 @@ CreateButton.addEventListener("click", event =>{
     let buttonElement = document.createElement("button")
     buttonElement.textContent="Delete"
     liElement.append(buttonElement);
-    console.log(buttonElement);
     buttonElement.addEventListener("click", function (){
         liElement.remove();
     })
 })
+
+//keyup event +Createbutton available
+
+InputElement.addEventListener("keydown", event =>{
+    if(event.target.value.length < 6){
+        InputElement.style.border = "2px solid red"}
+    else{
+        InputElement.style.border = "2px solid green";
+        CreateButton.removeAttribute("disabled");
+        }
+})
+
+
 
 
 
