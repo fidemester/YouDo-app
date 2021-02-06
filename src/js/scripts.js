e8b1b66fb96915632f59adc9c2e0f200eaa1206d
Krisztian <fodorkrisztian6@gmail.com>
2021-02-06 09:54:51 +0100

    MZ01,MZ02

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,17 @@
 //can be deleted
 console.log("Console log works!");
+let form
+let InputElement = document.getElementById("todo-text");
+
+let CreateButton = document.getElementById("create-btn")
+
+let UlElement = document.getElementById("ul-element")
+
+CreateButton.addEventListener("click", event =>{
+    event.preventDefault();
+    console.log(InputElement.value);
+    let liElement = document.createElement("li");
+    document.body.append(liElement);
+    liElement.innerText=InputElement.value;
+
+})
