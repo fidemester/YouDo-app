a35f39c376d12a2633714a0a41a59108910f577a
Krisztian <fodorkrisztian6@gmail.com>
2021-02-06 14:53:53 +0100

    MZ07, MZ08 -nem sikerült

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,44 +6,68 @@ console.log("Console log works!");
 let InputElement = document.getElementById("todo-text");
 let CreateButton = document.getElementById("create-btn");
 
+
 // create and remove list items, due date
 
-CreateButton.addEventListener("click", event =>{
+CreateButton.addEventListener("click", event => {
     event.preventDefault();
     let liElement = document.createElement("li");
     document.body.append(liElement);
-    liElement.innerText=InputElement.value;
+    liElement.innerText = InputElement.value;
 
     let ResetElement = document.createElement("button");
-   ResetElement.textContent="Delete"
-    liElement.append(ResetElement);
-    ResetElement.style.cursor="pointer";
+    ResetElement.textContent = "Delete"
+
+    ResetElement.style.cursor = "pointer";
 
     let dateElement = document.createElement("input");
-    dateElement.type="time";
-    document.body.append(dateElement);
+    dateElement.type = "time";
+
+    //list todos
+    liElement.append(dateElement, ResetElement);
 
 
+    //priority
 
+    let PriorityForm = document.getElementById("priority-form");
 
+    PriorityForm.addEventListener("click", event => {
+        console.log(event);
+        console.log(event.target.value);
+        switch (event.target.value) {
+            case "1":
+                let LowElement = document.createElement("h6").innerText = "Low";
+                liElement.append(LowElement);
+                break;
+            case "2":
+                let MediumElement = document.createElement("h5").innerText = "Medium";
+                liElement.append(MediumElement);
+                break;
 
-
-    // localStorage.setItem("Do list", JSON.stringify(liElement.innerText));
-
-    ResetElement.addEventListener("click", function (){
+            case "3":
+                let HeighElement = document.createElement("h4").innerText = "Heigh";
+                liElement.append(HeighElement);
+                break;
+            default:
+                console.log("nem jó");
+        }
+    })
+    ResetElement.addEventListener("click", function () {
         liElement.remove();
     })
-
 })
+
 
 //keyup event + Createbutton available
 
-InputElement.addEventListener("keydown", event =>{
-    if(event.target.value.length < 6){
-        InputElement.style.border = "2px solid red"}
-    else{
+InputElement.addEventListener("keydown", event => {
+    if (event.target.value.length < 6) {
+        InputElement.style.border = "2px solid red"
+    } else {
         InputElement.style.border = "2px solid green";
         CreateButton.removeAttribute("disabled");
-        }
+    }
 })
+
+
 
